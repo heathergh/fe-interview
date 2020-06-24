ae67fb75241563826516878111be65755741ee35
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-06-24 09:16:37 -0400

    Added note about Promise's param

--- sleep.js
+++ sleep.js
@@ -1,6 +1,7 @@
 // Create a sleep function that takes one parameter (time) and
 // will wait "time" ms
 
+// arguments that promise takes are called an executor
 const sleep = time => new Promise((resolve) => {
   setTimeout(()=>{
     resolve();
